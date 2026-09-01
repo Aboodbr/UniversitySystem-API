a77dbbc1df6b53bdf1cd621ff9fec84d08a9a6fd
Aboodbr <aboodragon12345@gmail.com>
2026-09-01 16:14:50 +0300

    Docs: Add full Postman collection for API testing

---
+++ 3. University.Infrastructure/Migrations/20260901125233_ApplyJulesUpdates.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace University.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplyJulesUpdates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Departments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Courses",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "CourseOfferings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Departments");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "CourseOfferings");
+        }
+    }
+}
